cc09ccf79f204ba9c1d9ccf4e9709fa196922443
Rickard Andersson <gonz@severnatazvezda.com>
2020-01-19 18:28:14 +0200

    add test rendering of pixels, scaled

--- src/main.zig
+++ src/main.zig
@@ -11,11 +11,39 @@ const ApplicationState = struct {
 
 const FileData = struct {
     name: []const u8,
+    width: u32,
+    height: u32,
     pixels: []Pixel,
     // this is meant to be a modifier for how big we need to draw pixels, as the user zooms in/out
     zoom_factor: f32,
+    surface: *c.SDL_Surface,
+
+    pub fn draw(self: FileData) void {
+        var y: u32 = 0;
+        while (y < self.height) : (y += 1) {
+            var x: u32 = 0;
+            while (x < self.width) : (x += 1) {
+                const pixel_index = (y * self.width) + x;
+                const pixel_width = application_width / self.width;
+                const pixel_height = application_height / self.height;
+                const pixel_rect = c.SDL_Rect{
+                    .x = @intCast(c_int, x * pixel_width),
+                    .y = @intCast(c_int, y * pixel_height),
+                    .w = @intCast(c_int, pixel_width),
+                    .h = @intCast(c_int, pixel_height),
+                };
+                const p = self.pixels[pixel_index];
+                _ = c.SDL_FillRect(
+                    self.surface,
+                    &pixel_rect,
+                    c.SDL_MapRGB(self.surface.format, p.r, p.g, p.b),
+                );
+            }
+        }
+    }
 
     // @TODO: add `saveToFile`
+    // @TODO: add `loadFromFile`
 };
 
 const Pixel = struct {
@@ -29,12 +57,6 @@ pub fn main() anyerror!void {
     var window: ?*c.SDL_Window = null;
     var surface: ?*c.SDL_Surface = null;
 
-    var application = ApplicationState{
-        .tick = 0,
-        .running = true,
-        .file_data = FileData{ .name = "test", .pixels = &[_]Pixel{}, .zoom_factor = 1.0 },
-    };
-
     if (c.SDL_Init(c.SDL_INIT_VIDEO) < 0) {
         const error_value = c.SDL_GetError();
         debug.warn("Unable to initialize SDL: {}\n", .{error_value});
@@ -44,8 +66,8 @@ pub fn main() anyerror!void {
             "PixEd",
             c.SDL_WINDOWPOS_UNDEFINED,
             c.SDL_WINDOWPOS_UNDEFINED,
-            width,
-            height,
+            application_width,
+            application_height,
             c.SDL_WINDOW_SHOWN,
         );
         if (window == null) {
@@ -55,6 +77,44 @@ pub fn main() anyerror!void {
             surface = c.SDL_GetWindowSurface(window);
         }
     }
+
+    var application = ApplicationState{
+        .tick = 0,
+        .running = true,
+        .file_data = FileData{
+            .name = "test",
+            .width = 2,
+            .height = 2,
+            .pixels = &[_]Pixel{
+                Pixel{
+                    .r = 0x00,
+                    .g = 0x00,
+                    .b = 0x00,
+                    .a = 0xff,
+                },
+                Pixel{
+                    .r = 0xff,
+                    .g = 0xff,
+                    .b = 0xff,
+                    .a = 0xff,
+                },
+                Pixel{
+                    .r = 0xff,
+                    .g = 0xff,
+                    .b = 0xff,
+                    .a = 0xff,
+                },
+                Pixel{
+                    .r = 0x00,
+                    .g = 0x00,
+                    .b = 0x00,
+                    .a = 0xff,
+                },
+            },
+            .zoom_factor = 1.0,
+            .surface = surface.?,
+        },
+    };
 
     var keyboard: [*]const u8 = undefined;
     while (application.running) : (application.tick += 1) {
@@ -80,8 +140,10 @@ fn update(application: *ApplicationState, keyboard: [*]const u8) void {
 fn render(window: *c.SDL_Window, surface: *c.SDL_Surface, application: ApplicationState) void {
     _ = c.SDL_FillRect(surface, null, c.SDL_MapRGB(surface.format, 0xff, 0xff, 0xff));
 
+    application.file_data.draw();
+
     _ = c.SDL_UpdateWindowSurface(window);
 }
 
-const width: u32 = 640;
-const height: u32 = 480;
+const application_width: u32 = 1280;
+const application_height: u32 = 720;
